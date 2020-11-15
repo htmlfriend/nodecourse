218355d17f041f0c297d77bbbe8cc0599e86718a
Yurii Tym <yuriy.tim@yahoo.com>
2020-11-14 19:10:19 -0500

    http requests

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-function task(x) {
-  let promise = new Promise((resolve, reject) => {
-    if (x < 18) {
-      let success = 'yes';
-      resolve(success);
-    } else {
-      let fail = new Error('no');
-      reject(fail);
-    }
-  });
-  let result = function () {
-    promise.then((full) => full).catch((err) => err);
-  };
-  result();
-}
+// function task(x) {
+//   let promise = new Promise((resolve, reject) => {
+//     if (x < 18) {
+//       let success = 'yes';
+//       resolve(success);
+//     } else {
+//       let fail = new Error('no');
+//       reject(fail);
+//     }
+//   });
+//   let result = function () {
+//     promise.then((full) => full).catch((err) => err);
+//   };
+//   result();
+// }
+const promisify = (func) => (...args) =>
+  new Promise((resolve, reject) =>
+    func(...args, (err, result) => (err ? reject(err) : resolve(result)))
+  );
+const delay = promisify((d, cb) => setTimeout(cb, d));
+delay(2000).then(() => console.log('Hi!'));
